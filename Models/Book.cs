4bbd3339d01d009faa1296c651c3c92d19350374
TheTomson <tomekdrom@interia.pl>
2022-02-15 22:01:11 +0100

    add validation to bookForm

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,20 +9,23 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		[Required(ErrorMessage ="Name is required!")]
 		[StringLength(255)]
 		public string Name { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Author name is required!")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre is required!")]
 		public Genre Genre { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre is required!")]
 		[Display(Name = "Genre")]
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+		[Required(ErrorMessage = "Release Date is required!")]
 		[Display(Name="Realease Date")]
 		public DateTime ReleaseDate { get; set; }
+		[Required(ErrorMessage = "NumberInStock is required and range is from 1-20!")]
+		[Range(1, 20)]
 		public int NumberInStock { get; set; }
 		public int NumberAvailable { get; set; }
 	}
